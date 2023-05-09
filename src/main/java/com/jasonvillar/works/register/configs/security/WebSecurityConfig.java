5bbe89153e9e4b258302ece1c2e36aebd73c821b
jasonsvillar <jason.villar633@gmail.com>
2023-05-09 11:43:43 -0300

    Spring Security: enable cors, disable csrf, enable basic Auth.

--- src/main/java/com/jasonvillar/works/register/configs/security/WebSecurityConfig.java
+++ src/main/java/com/jasonvillar/works/register/configs/security/WebSecurityConfig.java
@@ -2,11 +2,26 @@ package com.jasonvillar.works.register.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .httpBasic();
+        return http.build();
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
