56b0a6a6263dd8f4a168bd83d691238e1d020739
jasonsvillar <jason.villar633@gmail.com>
2023-05-09 11:42:48 -0300

    Fix code smells

--- src/main/java/com/jasonvillar/works/register/configs/security/SecurityUser.java
+++ src/main/java/com/jasonvillar/works/register/configs/security/SecurityUser.java
@@ -6,10 +6,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @AllArgsConstructor
 public class SecurityUser implements UserDetails {
-    private final User user;
+    private final transient User user;
 
     @Override
     public String getUsername() {
@@ -23,7 +24,7 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
