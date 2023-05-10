4a33b1cda7d03af8f0ab1a97ac2189df211743f6
jasonsvillar <jason.villar633@gmail.com>
2023-05-10 20:35:13 -0300

    Implementh OAuth2 with BasicAuthentication

--- src/main/java/com/jasonvillar/works/register/configs/security/WebSecurityConfig.java
+++ src/main/java/com/jasonvillar/works/register/configs/security/WebSecurityConfig.java
@@ -1,30 +1,26 @@
 package com.jasonvillar.works.register.configs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .csrf().disable()
-                .httpBasic()
-                .and()
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults());
-        return http.build();
+    @Autowired
+    SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors();
+        httpSecurity.csrf().disable();
+        httpSecurity.httpBasic();
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        httpSecurity.oauth2Login();
+        httpSecurity.formLogin();
+
+        return httpSecurity.build();
     }
 
     @Bean
