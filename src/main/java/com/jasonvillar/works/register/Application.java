7729fe70b95000c632ef6809ed4c57ada0fcecf5
jasonsvillar <jason.villar633@gmail.com>
2023-05-10 09:11:40 -0300

    Implement jasypt for encrypt/decrypt vars in application.properties

--- src/main/java/com/jasonvillar/works/register/Application.java
+++ src/main/java/com/jasonvillar/works/register/Application.java
@@ -1,9 +1,11 @@
 package com.jasonvillar.works.register;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
