c1d5ced2f28ac7c80fbb15db4ce299d59344b5f9
jasonsvillar <jason.villar633@gmail.com>
2023-10-12 16:52:36 -0300

    New Privilege Service + test

--- src/main/java/com/jasonvillar/works/register/services/PrivilegeService.java
+++ src/main/java/com/jasonvillar/works/register/services/PrivilegeService.java
@@ -1,13 +1,31 @@
-package com.jasonvillar.works.register.services.security;
+package com.jasonvillar.works.register.services;
 
+import com.jasonvillar.works.register.entities.Privilege;
 import com.jasonvillar.works.register.repositories.PrivilegeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PrivilegeService {
     private final PrivilegeRepository privilegeRepository;
 
-    
+    public List<Privilege> getList() {
+        return privilegeRepository.findAll();
+    }
+
+    public Privilege getById(long id) {
+        return privilegeRepository.findPrivilegeById(id);
+    }
+
+    public Optional<Privilege> getOptionalById(long id) {
+        return privilegeRepository.findOptionalById(id);
+    }
+
+    public List<Privilege> getListByNameLike(String name) {
+        return privilegeRepository.findAllByNameContainingIgnoreCase(name);
+    }
 }
---
+++ src/test/java/com/jasonvillar/works/register/services/PrivilegeServiceTest.java
@@ -0,0 +1,75 @@
+package com.jasonvillar.works.register.services;
+
+import com.jasonvillar.works.register.entities.Privilege;
+import com.jasonvillar.works.register.repositories.PrivilegeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class PrivilegeServiceTest {
+    @Mock
+    private PrivilegeRepository repository;
+
+    @InjectMocks
+    private PrivilegeService service;
+
+    private Privilege entity = Privilege.builder()
+            .name("Privilege Name")
+            .build();
+
+    @BeforeEach
+    void setup() {
+        entity.setId(1L);
+    }
+
+    @Test
+    void givenRepositories_whenGetList_thenReturnList() {
+        Mockito.when(repository.findAll()).thenReturn(List.of(entity));
+
+        List<Privilege> list = service.getList();
+
+        Assertions.assertThat(list).isNotEmpty();
+        Assertions.assertThat(list.get(0).getClass()).isEqualTo(Privilege.class);
+    }
+
+    @Test
+    void givenRepositories_whenGetById_thenReturnEntity() {
+        Mockito.when(repository.findPrivilegeById(1L)).thenReturn(entity);
+
+        Privilege entity = service.getById(1);
+
+        Assertions.assertThat(entity).isNotNull();
+        Assertions.assertThat(entity.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void givenRepositories_whenGetOptionalById_thenReturnOptional() {
+        Mockito.when(repository.findOptionalById(1L)).thenReturn(Optional.of(entity));
+        Mockito.when(repository.findOptionalById(3L)).thenReturn(Optional.empty());
+
+        Optional<Privilege> optional = service.getOptionalById(1);
+        Assertions.assertThat(optional).isPresent();
+
+        optional = service.getOptionalById(3);
+        Assertions.assertThat(optional).isNotPresent();
+    }
+
+    @Test
+    void givenRepositories_whenGetListByNameLike_thenReturnList() {
+        Mockito.when(repository.findAllByNameContainingIgnoreCase("name")).thenReturn(List.of(entity));
+
+        List<Privilege> list = service.getListByNameLike("name");
+
+        Assertions.assertThat(list).isNotEmpty();
+        Assertions.assertThat(list.get(0).getClass()).isEqualTo(Privilege.class);
+    }
+}
