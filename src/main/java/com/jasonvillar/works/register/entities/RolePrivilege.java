e77b9469d960624af7fa2d67f3a19f22017f66a0
jasonsvillar <jason.villar633@gmail.com>
2023-10-12 00:37:52 -0300

    New Entity RolePrivilege + liquibase scripts

---
+++ src/main/java/com/jasonvillar/works/register/entities/RolePrivilege.java
@@ -0,0 +1,27 @@
+package com.jasonvillar.works.register.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(uniqueConstraints = { @UniqueConstraint(name = "UniqueRoleAndPrivilege", columnNames = { "role_id", "privilege_id" }) })
+public class RolePrivilege {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "role_privilege_id_seq")
+    @SequenceGenerator(name = "role_privilege_id_seq", sequenceName = "role_privilege_id_seq", allocationSize = 1)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "privilege_id")
+    Privilege privilege;
+
+}
