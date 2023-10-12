829c1885ebc916b38e1465b57080813f35740251
jasonsvillar <jason.villar633@gmail.com>
2023-10-11 23:46:34 -0300

    New entity privilege + liquibase script

---
+++ src/main/java/com/jasonvillar/works/register/entities/Privilege.java
@@ -0,0 +1,30 @@
+package com.jasonvillar.works.register.entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Privilege {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "privilege_id_seq")
+    @SequenceGenerator(name = "privilege_id_seq", sequenceName = "privilege_id_seq", allocationSize = 1)
+    Integer id;
+
+    String name;
+
+    @Builder
+    public Privilege(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "privilege")
+    List<RolePrivilege> rolePrivilegeList;
+}
