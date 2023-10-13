582e24691c82d79d3bdd18a7a4e870f36386ae25
jasonsvillar <jason.villar633@gmail.com>
2023-10-12 23:12:10 -0300

    Role Service + test

--- src/test/java/com/jasonvillar/works/register/services/RoleServiceTest.java
+++ src/test/java/com/jasonvillar/works/register/services/RoleServiceTest.java
@@ -1,10 +1,18 @@
 package com.jasonvillar.works.register.services;
 
+import com.jasonvillar.works.register.entities.Role;
 import com.jasonvillar.works.register.repositories.RoleRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class RoleServiceTest {
@@ -13,4 +21,55 @@ public class RoleServiceTest {
 
     @InjectMocks
     private RoleService service;
+
+    private Role entity = Role.builder()
+            .name("Role Name")
+            .build();
+
+    @BeforeEach
+    void setup() {
+        entity.setId(1L);
+    }
+
+    @Test
+    void givenRepositories_whenGetList_thenReturnList() {
+        Mockito.when(repository.findAll()).thenReturn(List.of(entity));
+
+        List<Role> list = service.getList();
+
+        Assertions.assertThat(list).isNotEmpty();
+        Assertions.assertThat(list.get(0).getClass()).isEqualTo(Role.class);
+    }
+
+    @Test
+    void givenRepositories_whenGetById_thenReturnEntity() {
+        Mockito.when(repository.findRoleById(1L)).thenReturn(entity);
+
+        Role entity = service.getById(1);
+
+        Assertions.assertThat(entity).isNotNull();
+        Assertions.assertThat(entity.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void givenRepositories_whenGetOptionalById_thenReturnOptional() {
+        Mockito.when(repository.findOptionalById(1L)).thenReturn(Optional.of(entity));
+        Mockito.when(repository.findOptionalById(3L)).thenReturn(Optional.empty());
+
+        Optional<Role> optional = service.getOptionalById(1);
+        Assertions.assertThat(optional).isPresent();
+
+        optional = service.getOptionalById(3);
+        Assertions.assertThat(optional).isNotPresent();
+    }
+
+    @Test
+    void givenRepositories_whenGetListByNameLike_thenReturnList() {
+        Mockito.when(repository.findAllByNameContainingIgnoreCase("name")).thenReturn(List.of(entity));
+
+        List<Role> list = service.getListByNameLike("name");
+
+        Assertions.assertThat(list).isNotEmpty();
+        Assertions.assertThat(list.get(0).getClass()).isEqualTo(Role.class);
+    }
 }
