2fbd054efaa818664da038fe4198b2fad4ed9ec5
jasonsvillar <jason.villar633@gmail.com>
2023-10-20 23:58:31 -0300

    Delete OAuth2 authentication

--- src/main/java/com/jasonvillar/works/register/security/WebSecurityConfig.java
+++ src/main/java/com/jasonvillar/works/register/security/WebSecurityConfig.java
@@ -49,9 +49,9 @@ public class WebSecurityConfig {
                         .anyRequest().permitAll()
         );
 
-        // For OAuth2 Link = http://localhost:8080/oauth2/authorization/github
-        httpSecurity.oauth2Login()
-                .loginPage("/api/auth/authentication-required");
+//        For OAuth2 Link = http://localhost:8080/oauth2/authorization/github
+//        httpSecurity.oauth2Login()
+//                .loginPage("/api/auth/authentication-required");
 
         httpSecurity.authenticationProvider(authenticationProvider()); // For implement custom DAO Basic Auth
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
