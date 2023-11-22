e941b17b60f9306bec4e4bd0fa61f7541ac10659
jasonsvillar <jason.villar633@gmail.com>
2023-11-22 00:29:27 -0300

    Exeption for not send mail

--- src/main/java/com/jasonvillar/works/register/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jasonvillar/works/register/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jasonvillar.works.register.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSendException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,4 +40,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(Exception ex) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(ex.getMessage());
     }
+
+    @ExceptionHandler({ MailSendException.class })
+    @ResponseBody
+    public ResponseEntity<String> handleMailSendException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
 }
