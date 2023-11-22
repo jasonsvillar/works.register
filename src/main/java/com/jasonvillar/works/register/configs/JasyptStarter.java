eab7229efe05a7d60c11a2a8dc2efb0fb0b3a31b
jasonsvillar <jason.villar633@gmail.com>
2023-11-22 11:47:45 -0300

    Jasypt for render profile

---
+++ src/main/java/com/jasonvillar/works/register/configs/JasyptStarter.java
@@ -0,0 +1,9 @@
+package com.jasonvillar.works.register.configs;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptStarter {
+}
